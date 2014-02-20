55232e5e77af4c7dacfe11dc35cc1578d9ba26cd
samuel gonzalez <alu0100536805@ull.edu.es>
2014-02-20 20:28:27 +0000

    Modificando los tests

--- test/tests.js
+++ test/tests.js
@@ -1,26 +1,23 @@
 var assert = chai.assert;
 
 suite('Resultados de los tests:', function() {
-    test('Detección correcta de header', function() {
+    test('Detecta un header', function() {
         var tokens = lexer('[HOLA]');
 assert.equal(tokens[0].type,'header');
     });
-    test('Detección correcta de blanco', function() {
+    test('Detecta un blanco', function() {
         var tokens = lexer(' ');
 assert.equal(tokens[0].type,'blanks');
     });
-    test('Detección correcta de comentario', function() {
-        var tokens = lexer('; Esto es un comentario ');
+    test('Detecta un Comentario', function() {
+        var tokens = lexer('; Aqui un ejempl de comentario ');
 assert.equal(tokens[0].type,'comments');
     });
-    test('Detección correcta de asignación', function() {
-        var tokens = lexer('Nombre = Antonio');
+    test('Detecta una asignacion', function() {
+        var tokens = lexer('Edad = 33');
 assert.equal(tokens[0].type,'nameEqualValue');
     });
-    test('Detección correcta de un error', function() {
-        var tokens = lexer('@!¿¡?');
-assert.equal(tokens[0].type,'error');
-    });
+
 
 
 });
